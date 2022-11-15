6d57ace4eafca4f418872cc6aa6c0c9fec7363f2
Jamie <adams.j.6385@gmail.com>
2022-11-15 23:12:59 +0000

    Data moved to own JS file. Static paths for data and basic layout for each review

---
+++ src/data/reviews.js
@@ -0,0 +1,32 @@
+const slug = (brand) => {
+    return brand
+}
+
+export const reviews = [
+	{
+		id: 0,
+		title: 'First Tesco Own',
+		brand: 'Tesco',
+		range: '',
+		image: '/images/Tesco Default.jpg',
+		published: new Date("2022-10-25"),
+		description: 'Good flavour, although a lot of pastry.  Good value.',
+		rating: {
+			overal: 13.5
+		},
+        slug: 'reviews/tesco'
+	},
+	{
+		id: 1,
+		title: 'Second Tesco Own',
+		brand: 'Mr Kipling',
+		range: '',
+		image: '/images/Mr Kipling Default.jpg',
+		published: new Date("2022-11-02"),
+		description: 'Very disappointing, flavourless paste of a filling.  Nice-looking box but why the plastic? We don\'t want plastic.',
+		rating: {
+			overal: 8
+		},
+        slug: 'reviews/mr-kipling'
+	}
+]
