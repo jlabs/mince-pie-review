4706666f25f091f98a4914b6f04f6188398b861a
Jamie <adams.j.6385@gmail.com>
2022-11-16 21:55:39 +0000

    More data fields added

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -5,28 +5,44 @@ const slug = (brand) => {
 export const reviews = [
 	{
 		id: 0,
-		title: 'First Tesco Own',
 		brand: 'Tesco',
 		range: '',
 		image: '/images/Tesco Default.jpg',
 		published: new Date("2022-10-25"),
 		description: 'Good flavour, although a lot of pastry.  Good value.',
+		overal: 13.5,
 		rating: {
-			overal: 13.5
+			pastry: 0,
+			filling: 0,
+			presentation: 0,
+			value: 0
 		},
-        slug: 'tesco'
+        slug: 'tesco',
+		agregregated: true,
+		reviewers: 5,
+		served: 'Room Temperature',
+		side: '',
+		price: 0.00,
 	},
 	{
 		id: 1,
-		title: 'Second Tesco Own',
 		brand: 'Mr Kipling',
 		range: '',
 		image: '/images/Mr Kipling Default.jpg',
 		published: new Date("2022-11-02"),
 		description: 'Very disappointing, flavourless paste of a filling.  Nice-looking box but why the plastic? We don\'t want plastic.',
+		overal: 8,
 		rating: {
-			overal: 8
+			pastry: 0,
+			filling: 0,
+			presentation: 0,
+			value: 0
 		},
-        slug: 'mr-kipling'
-	}
+        slug: 'mr-kipling',
+		agregregated: true,
+		reviewers: 5,
+		served: 'Room Temperature',
+		side: '',
+		price: 0.00,
+	},	
 ]
