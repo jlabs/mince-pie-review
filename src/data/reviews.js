5f444e1417c2ec1ad6d35655475d8dabee61651c
Jamie <adams.j.6385@gmail.com>
2022-11-16 23:38:22 +0000

    Added more reviews

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -4,7 +4,6 @@ const slug = (brand) => {
 
 export const reviews = [
 	{
-		id: 0,
 		brand: 'Tesco',
 		range: '',
 		image: '/images/Tesco Default.jpg',
@@ -25,11 +24,10 @@ export const reviews = [
 		price: 0.00,
 	},
 	{
-		id: 1,
 		brand: 'Mr Kipling',
 		range: '',
 		image: '/images/Mr Kipling Default.jpg',
-		published: new Date("2022-11-02"),
+		published: new Date("2022-10-25"),
 		description: 'Very disappointing, flavourless paste of a filling.  Nice-looking box but why the plastic? We don\'t want plastic.',
 		overal: 8,
 		rating: {
@@ -44,5 +42,45 @@ export const reviews = [
 		served: 'Room Temperature',
 		side: '',
 		price: 0.00,
-	},	
+	},
+	{
+		brand: 'Sainsbury\'s',
+		range: '',
+		image: '/images/Mr Kipling Default.jpg',
+		published: new Date("2022-11-02"),
+		description: 'Theses are bad. 7.5/20 and that\'s being generous.  Boring, tasteless and textureless. No plastic on the box itself, but a weak design on the outside.',
+		overal: 7.5,
+		rating: {
+			pastry: 0,
+			filling: 0,
+			presentation: 0,
+			value: 0
+		},
+        slug: 'sainsburys',
+		agregregated: true,
+		reviewers: 5,
+		served: 'Room Temperature',
+		side: '',
+		price: 1.25,
+	},
+	{
+		brand: 'Sainsbury\'s',
+		range: 'Taste The Difference',
+		image: '/images/Mr Kipling Default.jpg',
+		published: new Date("2022-11-02"),
+		description: 'At £2.25 these are a whole pound dearer. Worth it.  A potential champion here with a massive 17/20. Delicious, textured filling and proper pastry that didn\'t crumble. We loved the actual icing sugar sprinkling - one taster remarked \'It looks like snowy Christmas\'. ',
+		overal: 17,
+		rating: {
+			pastry: 0,
+			filling: 0,
+			presentation: 0,
+			value: 0
+		},
+        slug: 'sainsburys-taste-the-difference',
+		agregregated: true,
+		reviewers: 5,
+		served: 'Room Temperature',
+		side: '',
+		price: 2.25,
+	},
 ]
